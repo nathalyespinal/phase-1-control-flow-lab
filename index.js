c8fdef6f31c83e8fbc2999966282b9d9c39a024e
NathalyEspinal <espinalnathaly@gmail.com>
2022-02-22 17:23:23 -0600

    commit tree 432ec14d0dbdd3f5505f7e10f76d192bca4f6492

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return 'This one is on me!'
+} else if (distance > 2000 && distance < 2500) {
+   return 'I will gladly take your thirty bucks.'
+} else if (distance > 2500 ) {
+    return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+(city === NYC)? ("Ok, sounds good.") : ("No go.")
 }
